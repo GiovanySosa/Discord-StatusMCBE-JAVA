1c244b6337c6d6b8d28b5352a9211fd0ba9fa078
Gio <80273507+GioUwU@users.noreply.github.com>
2021-09-10 15:04:57 -0500

    code fix

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import discord
 import os
+from discord.ext import commands
 from dotenv import load_dotenv
-from mcstats import mcstats
+from mcpe_query.query import Query
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -10,31 +11,28 @@ bot = commands.Bot(command_prefix='!')
 
 
 @bot.command()
-async def status(ctx,):
-    host = 'yourip'
-    port = 19132 #yourport
+async def status(ctx, ):
+    host = 'monsterhcf.ddns.net'
+    port = 19132  # yourport
     q = Query(host, port)
     data = q.query()
     online = data.num_players
     max = data.max_players
     jugadores = str(data.players)
-    
-    
 
-    embed = discord.Embed(title="MonsterNetwork", description=f"**IP:** {host} **PUERTO:** {port}",color=discord.Color.random())
-    
-    embed.add_field(name="HCF:", value=f"*ACTIVOS:** {online} **de** {max} \n \n **JUGADORES:** {jugadores}", inline=False)
-    
+    embed = discord.Embed(title="MonsterNetwork", description=f"**IP:** {host} **PUERTO:** {port}",
+                          color=discord.Color.random())
+
+    embed.add_field(name="HCF:", value=f"*ACTIVOS:** {online} **de** {max} \n \n **JUGADORES:** {jugadores}",
+                    inline=False)
+
     embed.add_field(name="**invita a tus amigos**", value='[**TOCA AQUI PARA INVITAR**](linkinviteordiscord)')
-    
+
     embed.set_footer(text=f" **juega y divierte te!!** ")
 
-    await channel.send(embed=embed)
+    await ctx.send(embed=embed)
     print(jugadores)
 
 
-
-
-
-
 bot.run(TOKEN)
+
